0790981c0c20bf69886aac6a977a6dfcfffe7f7d
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-11-21 13:49:47 -0600

    Moved Components folder to src, created basic header file and added it to layout file. Also updated sanity/vision to 4.18

---
+++ nextjs-weareiw/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+//this is base for the header, not final
+export default function Header() {
+    return (
+        <header>
+            <Link href="/"></Link>
+            <Link href="#">Products</Link>
+            <Link href="#">Services</Link>
+            <Link href="#">About</Link>
+            <Link href="#">Blog</Link>
+            <Link href="#">Careers</Link>
+        </header>
+    )
+}
