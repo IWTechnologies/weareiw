9d5e8d135096af4e6be6f17df364807815a6272e
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-11-21 17:00:18 -0600

    Header: more styling updates, still messing around with this. Globals: added spacing and other brand colors to theme.

--- nextjs-weareiw/src/components/Header.tsx
+++ nextjs-weareiw/src/components/Header.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 //this is base for the header, not final
 export default function Header() {
     return (
-        <header className="bg-brand-navy text-white">
-            <Link href="/"></Link>
-            <Link href="#">Products</Link>
-            <Link href="#">Services</Link>
-            <Link href="#">About</Link>
-            <Link href="#">Blog</Link>
-            <Link href="#">Careers</Link>
+        <header className="h-123 bg-brand-navy font-semibold text-2xl text-white">
+            <nav className="flex items-center justify-end space-x-8">
+                <Link href="/"></Link>
+                <Link href="#">Products</Link>
+                <Link href="#">Services</Link>
+                <Link href="#">About</Link>
+                <Link href="#">Blog</Link>
+                <Link href="#">Careers</Link>
+            </nav>
         </header>
     )
 }
