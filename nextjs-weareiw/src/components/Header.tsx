05a169d992d6c117ff5f396ddf65f6b846f641b9
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-11-24 15:29:17 -0600

    Header: added main IW logo, working on structure and styling.

--- nextjs-weareiw/src/components/Header.tsx
+++ nextjs-weareiw/src/components/Header.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
 
 //this is base for the header, not final
 export default function Header() {
     return (
-        <header className="h-123 bg-brand-navy font-semibold text-2xl text-white">
-            <nav className="flex items-center justify-end space-x-8">
-                <Link href="/"></Link>
-                <Link href="#">Products</Link>
-                <Link href="#">Services</Link>
-                <Link href="#">About</Link>
-                <Link href="#">Blog</Link>
-                <Link href="#">Careers</Link>
-            </nav>
+        <header className="h-123 flex items-center bg-brand-navy font-semibold text-2xl text-white">
+            <div className="h-full w-[95%] mx-auto flex justify-between items-center">
+                <div>
+                    <Link href="/">
+                        <Image src="/iw-logo.png" alt="IW Technologies Logo" width={300} height={123} />
+                    </Link>
+                </div>
+                <nav className="flex items-center justify-end space-x-8">
+                    <Link href="#">Products</Link>
+                    <Link href="#">Services</Link>
+                    <Link href="#">About</Link>
+                    <Link href="#">Blog</Link>
+                    <Link href="#">Careers</Link>
+                    <Link href="#" className="bg-brand-aqua text-brand-navy">Get In Touch</Link>
+                </nav>
+            </div>
         </header>
     )
 }
