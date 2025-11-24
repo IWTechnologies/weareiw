18d1672dd727ca1c0fa56c7c054c9f7451af1283
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-11-24 14:06:31 -0600

    Updated fonts

--- nextjs-weareiw/src/app/layout.tsx
+++ nextjs-weareiw/src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Montserrat, Open_Sans } from "next/font/google";
 import Header from "@/components/Header";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const montserrat = Montserrat({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+})
+const openSans = Open_Sans({
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "IW Technologies - Your Enterprise Solution Partner",
@@ -26,8 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${montserrat.className} ${openSans.className} antialiased`}>
         <Header />
         {children}
       </body>
