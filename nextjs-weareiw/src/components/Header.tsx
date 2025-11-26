fadff9da3d44944aa9d2457a02772b3371eb387e
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-11-26 14:46:08 -0600

    Header: made iw logo div wrapper a flex container, added set max height for optimization. updated sanity local version to match runtime version.

--- nextjs-weareiw/src/components/Header.tsx
+++ nextjs-weareiw/src/components/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
     return (
         <header className="sticky top-0 h-[123px] flex items-center bg-brand-navy font-semibold text-xl text-white">
             <div className="h-full w-[95%] mx-auto flex justify-between items-center">
-                <div>
+                <div className="h-full flex items-center">
                     <Link href="/">
-                        <Image src="/iw-logo.png" alt="IW Technologies Logo" width={300} height={0} className="h-auto w-auto" />
+                        <Image className="max-h-[70px] w-auto object-contain" priority width={1047} height={205} src="/iw-logo.png" alt="IW Technologies Logo" />
                     </Link>
                 </div>
                 <nav className="flex items-center justify-end space-x-8">
