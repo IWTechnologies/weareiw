5a160cf5c03dfbec5ee0dd9dd0593eb817312fe7
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-12-03 01:50:52 -0600

    Footer and Card: updated a tags in footer to be Links, content added to card component and added to page. Still needs more styling done.

--- nextjs-weareiw/src/app/page.tsx
+++ nextjs-weareiw/src/app/page.tsx
@@ -3,6 +3,7 @@ import { type SanityDocument } from "next-sanity";
 
 import { client } from "@/sanity/lib/client";
 // this is a comment
+import Card from "@/components/Card";
 
 const POSTS_QUERY = `*[
   _type == "post"
@@ -27,6 +28,31 @@ export default async function IndexPage() {
           </li>
         ))}
       </ul>
+      <section className="w-full flex">
+        <Card 
+          imageSrc="/View-our-products-Moody-blue.png"
+          imageAlt="Blue tinted view our products card image"
+          imageWidth={663}
+          imageHeight={432}
+          title="Products We Stand Behind"
+          text="From displays to scanners to printers, we offer a wide range
+          of new and expertly refurbished IT assets from a variety of manufacturers."
+          linkHref="#"
+          linkLabel="View Our Products"
+        />
+        <Card 
+          imageSrc="/Services-Moody-Blue.png"
+          imageAlt="Blue tinted Services card image"
+          imageWidth={669}
+          imageHeight={375}
+          title="Services for Every Stage"
+          text="From installation to on-site repair to e-cycling, our service
+          offerings can cover the full lifecycle of your technology or be chosen a la carte."
+          linkHref="#"
+          linkLabel="Explore Our Services"
+        />
+      </section>
+      
     </main>
   );
 }
--- nextjs-weareiw/src/components/Card.tsx
+++ nextjs-weareiw/src/components/Card.tsx
@@ -1,13 +1,25 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Card() {
+type CardProps = {
+    imageSrc: string;
+    imageAlt: string;
+    imageWidth: number;
+    imageHeight: number;
+    title: string;
+    text: string;
+    linkHref: string;
+    linkLabel: string;
+};
+
+export default function Card({ imageSrc, imageAlt, imageWidth, imageHeight, title, text, linkHref, linkLabel }: CardProps) {
     return (
         <div className="flex flex-col">
-            <div>
-                <Image className="max-h-[250px] h-auto w-auto object-contain" 
-                src="/Services-Moody-Blue.png" alt="Services card image" width={669} height={375} />
-            </div>
+            <Image className="max-h-[250px] h-auto w-auto object-contain" 
+            src={imageSrc} alt={imageAlt} width={imageWidth} height={imageHeight} />
+            <h2>{title}</h2>
+            <p>{text}</p>
+            <Link href={linkHref}>{linkLabel}</Link>
         </div>
     )
 }
