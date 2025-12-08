8048f65c1f5fc3a6f8905e413bf15be0ab561288
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-12-08 16:35:39 -0600

    Updated home page to remove test posts. Placeholder text for hero section

--- nextjs-weareiw/src/app/page.tsx
+++ nextjs-weareiw/src/app/page.tsx
@@ -1,33 +1,14 @@
 import Link from "next/link";
-import { type SanityDocument } from "next-sanity";
+//import { type SanityDocument } from "next-sanity";
+//import { client } from "@/sanity/lib/client";
 
-import { client } from "@/sanity/lib/client";
-// this is a comment
 import Card from "@/components/Card";
 
-const POSTS_QUERY = `*[
-  _type == "post"
-  && defined(slug.current)
-]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
 
-const options = { next: { revalidate: 30 } };
-
-export default async function IndexPage() {
-  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
-
+export default function IndexPage() {
   return (
     <main className="container mx-auto min-h-screen max-w-9/10 p-8">
-      <h1 className="text-4xl font-bold mb-8">These are test posts</h1>
-      <ul className="flex flex-col gap-y-4">
-        {posts.map((post) => (
-          <li className="hover:underline" key={post._id}>
-            <Link href={`/${post.slug.current}`}>
-              <h2 className="text-xl font-semibold">{post.title}</h2>
-              <p>{new Date(post.publishedAt).toLocaleDateString()}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-4xl font-bold mb-8">Hero section will go here</h1>
       <section className="w-full flex space-x-4">
         <Card 
           imageSrc="/View-our-products-Moody-blue.png"
