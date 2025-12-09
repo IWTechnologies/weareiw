a668117ee8deac10ff0368b84e92e22246254ce4
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-12-09 15:27:25 -0600

    Card: more styling updates

--- nextjs-weareiw/src/components/Card.tsx
+++ nextjs-weareiw/src/components/Card.tsx
@@ -14,10 +14,10 @@ type CardProps = {
 
 export default function Card({ imageSrc, imageAlt, imageWidth, imageHeight, title, text, linkHref, linkLabel }: CardProps) {
     return (
-        <div className="w-1/2 flex flex-col border-2 border-solid shadow-lg">
+        <div className="w-lg flex flex-col border-2 border-solid shadow-lg">
             <Image className="max-h-[250px] h-auto w-auto object-cover" 
             src={imageSrc} alt={imageAlt} priority width={imageWidth} height={imageHeight} />
-            <div className="p-8 text-brand-navy">
+            <div className="flex flex-col p-8 text-brand-navy">
                 <h2 className="pb-2 text-3xl font-bold">{title}</h2>
                 <p className="font-light text-2xl">{text}</p>
                 <Link href={linkHref} className="px-[.3em] py-[1em] text-xl bg-brand-aqua">{linkLabel}</Link>
