f9036be9f7d70b3eb63a4fe11c2d3bbd9bcebf6c
Antonio Saenz <asaenz@illinoiswholesale.com>
2025-12-09 16:52:48 -0600

    Card: container alignment and padding updated

--- nextjs-weareiw/src/components/Card.tsx
+++ nextjs-weareiw/src/components/Card.tsx
@@ -17,10 +17,10 @@ export default function Card({ imageSrc, imageAlt, imageWidth, imageHeight, titl
         <div className="w-lg flex flex-col border-2 border-solid shadow-lg">
             <Image className="max-h-[250px] h-auto w-auto object-cover" 
             src={imageSrc} alt={imageAlt} priority width={imageWidth} height={imageHeight} />
-            <div className="flex flex-col p-8 text-brand-navy">
+            <div className="flex flex-col items-start p-8 text-brand-navy">
                 <h2 className="pb-2 text-3xl font-bold">{title}</h2>
-                <p className="font-light text-2xl">{text}</p>
-                <Link href={linkHref} className="px-[.3em] py-[1em] text-xl bg-brand-aqua">{linkLabel}</Link>
+                <p className="pb-5 font-light text-2xl">{text}</p>
+                <Link href={linkHref} className="px-[1em] py-[.33em] border-brand-aqua text-[22px] bg-brand-aqua">{linkLabel}</Link>
             </div>
         </div>
     )
