41f2f18d27a7b81af812caa35cd1378b63686ca5
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-01-29 14:22:27 -0600

    Header: starting to add mobile navbar, still needs a lot of work

--- nextjs-weareiw/src/components/header/Header.tsx
+++ nextjs-weareiw/src/components/header/Header.tsx
@@ -1,10 +1,15 @@
+"use client"
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 import { navItems } from "@/data/navItems";
 import NavItem from "./NavItem";
 
 //this is base for the header, not final
 export default function Header() {
+    const [mobileOpen, setMobileOpen] = useState(false);
+
     return (
         <header className="sticky top-0 z-50 h-[123px] flex items-center bg-brand-navy font-semibold text-xl text-white">
             <div className="h-full w-[95%] mx-auto flex justify-between items-center">
@@ -15,13 +20,27 @@ export default function Header() {
                         src="/iw-logo.png" alt="IW Technologies Logo" />
                     </Link>
                 </div>
-                <nav className="flex items-center space-x-6">
+
+                {/* Desktop Nav hidden until lg breakpoint*/}
+                <nav className="hidden lg:flex items-center space-x-6">
                     {navItems.map((item) => (
                         <NavItem key={item.label} {...item} />
                     ))}
                     <Link href="#" className="transition duration-300 ease-in-out
                     bg-brand-aqua text-brand-navy">Get In Touch</Link>
                 </nav>
+
+                {/* Mobile Nav */}
+                {/* Burger Button mobile only */}
+                <button
+                onClick={() => setMobileOpen((prev) => !prev)}
+                className="lg:hidden flex flex-col gap-1"
+                aria-label="Toggle menu"
+                >
+                <span className="w-6 h-[2px] bg-white" />
+                <span className="w-6 h-[2px] bg-white" />
+                <span className="w-6 h-[2px] bg-white" />
+                </button>
             </div>
         </header>
     )
