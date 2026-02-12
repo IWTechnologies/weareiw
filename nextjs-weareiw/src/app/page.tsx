af7d875091e675b448f720ace93e56c25f95bacb
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-02-12 11:15:34 -0600

    Homepage: added new subheading to card section.

--- nextjs-weareiw/src/app/page.tsx
+++ nextjs-weareiw/src/app/page.tsx
@@ -55,8 +55,13 @@ export default function IndexPage() {
       </section>
       
       <section 
-      className="w-[80%] mx-auto flex flex-col items-center">
-        <div className="w-full flex flex-col items-center gap-8
+      className="w-[100%] flex flex-col items-center">
+        <div className="w-full pb-10 font-bold text-3xl lg:text-4xl text-brand-black text-center">
+          <h2>
+            Start with hardware. Or start with services. We’re built to scale with you.
+          </h2>
+        </div>
+        <div className="w-[80%] mx-auto flex flex-col items-center gap-8
         lg:flex-row lg:justify-center lg:items-start">
           <Card 
             imageSrc="/View-our-products-Moody-blue.png"
