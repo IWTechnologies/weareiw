c97b484d1889999027a5229fce6271b3292032d7
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-02-25 16:51:07 -0600

    PromoBar: styles and responsiveness added, gold color added to css file.

--- nextjs-weareiw/src/components/PromoBar.tsx
+++ nextjs-weareiw/src/components/PromoBar.tsx
@@ -2,8 +2,16 @@ import Link from "next/link";
 
 export default function PromoBar() {
     return (
-        <div className="w-full flex justify-center bg-brand-aqua text-brand-black">
-            test test test
+        <div className="w-full flex justify-center text-center bg-brand-gold text-brand-black">
+            <div className="w-[80%] md:w-full mx-auto flex flex-col md:flex-row items-center md:justify-center">
+                <span className="w-fit">50 Years. Built on Trust. Earned in the Field...</span>
+                <Link href="https://www.weareiw.com/about/our-story/" 
+                target="_blank" 
+                className="w-fit font-medium underline hover:text-white hover:cursor-pointer">
+                    See our story
+                </Link>
+            </div>
+            
         </div>
     )
 }
