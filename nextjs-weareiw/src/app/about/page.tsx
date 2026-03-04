38bd0e6fdd5639b68cc7d9719e0cdbc79d79f0ff
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-03-04 16:37:08 -0600

    Created folders and files for services and about pages. both have the same placeholder Hero component

---
+++ nextjs-weareiw/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import ProductsHero from "@/components/ProductsHero";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <section className="">
+                <ProductsHero />
+            </section> 
+        </div>
+    )
+}
---
+++ nextjs-weareiw/src/app/services/page.tsx
@@ -0,0 +1,11 @@
+import ProductsHero from "@/components/ProductsHero";
+
+export default function ServicesPage() {
+    return (
+        <div>
+            <section className="">
+                <ProductsHero />
+            </section> 
+        </div>
+    )
+}
