88b89bb2b7686500f52875f80f034b262c92dec7
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-03-25 11:00:52 -0500

    our-story: continued work on page

--- nextjs-weareiw/src/app/about/our-story/page.tsx
+++ nextjs-weareiw/src/app/about/our-story/page.tsx
@@ -37,7 +37,7 @@ export default function OurStory() {
                 />
             </section>
 
-            <section className="flex justify-center p-5 bg-brand-black text-white">
+            <section className="flex justify-center py-10 bg-brand-black text-white">
                 <div>
                     <h2 className="border-b-4 border-brand-aqua p-2">
                         FOLLOW ALONG WITH US
@@ -45,15 +45,15 @@ export default function OurStory() {
                 </div>
             </section>
 
-            <section className="w-full flex justify-center bg-brand-black text-brand-black">
-                <div className="w-full py-14 mt-4 rounded-t-3xl bg-background">
-                    <div className="w-[70%] mx-auto flex flex-col gap-5">
-                        <h2 className="w-full border-b-2 border-brand-black p-2 text-brand-navy">
+            <section className="w-full flex justify-center bg-brand-black text-white">
+                <div className="w-full py-14 mt-14">
+                    <div className="w-[70%] mx-auto flex flex-col">
+                        <h2 className="w-full border-b-2 border-brand-black p-2">
                             THE BEGINNING
                         </h2>
-                        <div className="relative">
-                            <div className="timeline-container w-full p-2">
-                                <div className="seventyFour flex">
+                        <div className="relative py-16 pt-0 mt-16">
+                            <div className="timeline-container w-full flex flex-col items-center">
+                                <div className="seventyFour w-full flex">
                                     <div 
                                     className="w-1/2 h-fit flex flex-col items-center p-5 mr-[5.5%] 
                                     rounded-r-full bg-brand-navy text-white">
@@ -89,11 +89,23 @@ export default function OurStory() {
                                         </div>
                                     </div>
                                 </div> {/* end of seventyFour container */}
-                                
                             </div> {/* end of timeline container */}
-                        </div>
+                        </div> {/* end of relative container */}
+
+                        <div className="relative py-16">
+                            <div className="timeline-container w-full flex flex-col items-center">
+                                <div className="alMoor flex justify-center">
+                                    <div className="w-full sm:w-[80%] z-10">
+                                        <Image
+                                        className="w-full h-auto object-contain rounded-3xl" 
+                                        src="/heroImages/shutterstock1.jpg" alt="What's in it for you image graphic" 
+                                        width="1920" height="1080"
+                                        sizes="(max-width: 1920px) 100vw, 50vw" />
+                                    </div>
+                                </div> {/* end of al image container */}
+                            </div> {/* end of timeline container 2 */}
+                        </div>{/* end of relative container */}
                     </div>
-                    
                 </div>
             </section>
         </div>
