4af4714a389608a2762e24063cecc48909aa91ee
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-04-22 09:43:26 -0500

    NumCounter: removed old unused code

--- nextjs-weareiw/src/components/NumCounter.tsx
+++ nextjs-weareiw/src/components/NumCounter.tsx
@@ -15,21 +15,6 @@ type StatSection = {
     stats: Stat[];
 };
 
-// const rowOne: Stat[] = [
-//     { value: 240000, label: "Sq Ft of Warehouse Space" },
-//     { value: 18000, label: "Pallet Storage Locations" },
-//     { value: 50, label: "Years in Business" },
-//     { value: 40000, label: "Annual Shipments" },
-//     { value: 7500, label: "Customers Nationwide", suffix: "+" }
-// ];
-
-// const rowTwo: Stat[] = [
-//     { value: 35000, label: "Checkout Lanes Supported", suffix: "+" },
-//     { value: 55000, label: "On-Site Service Events (2024)", suffix: "+" },
-//     { value: 9650, label: "Locations Supported Across North America (2024)" },
-//     { value: 1000, label: "Rapid-Scale Deployments in Under 8 Weeks", suffix: "+" }
-// ];
-
 function StatItem({ value, label, subLabel, suffix = "" }: Stat) {
     const [visible, setVisible] = useState(false);
     const [display, setDisplay] = useState(0);
@@ -89,11 +74,6 @@ export default function StatSection({ title, subTitle, stats }: StatSection) {
                 <h2>{title}</h2>
                 <div>{subTitle}</div>
             </div>
-            {/* <div className="w-full mb-8 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 gap-8">
-                {rowOne.map((stat) => (
-                    <StatItem key={stat.label} {...stat} />
-                ))}
-            </div> */}
             <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {stats.map((stat) => (
                     <StatItem key={stat.label} {...stat} />
