e2c19f45b275bf637687c9404aaed055bab18f3e
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-05-21 16:59:00 -0500

    job: added new queries. created job types file. still not finished.

--- nextjs-weareiw/src/sanity/queries/job.ts
+++ nextjs-weareiw/src/sanity/queries/job.ts
@@ -1,23 +1,53 @@
 import { groq } from "next-sanity";
 
-export const fetchAllJobs = groq`
-    *[_type == "jobs" && active == true] {
-        _id,
-        jobTitle,
-        "slug": slug.current,
-        category,
-        "jobLocation": jobLocation.city + ", " + jobLocation.state,
-        type,
-        jobDescription,
-    }
-`;
+// export const fetchAllJobs = groq`
+//     *[_type == "jobs" && active == true] {
+//         _id,
+//         jobTitle,
+//         "slug": slug.current,
+//         category,
+//         "jobLocation": jobLocation.city + ", " + jobLocation.state,
+//         type,
+//         jobDescription,
+//     }
+// `;
 
-export const fetchJobCategories = groq`
-    *[_type == "jobCategory"] | order(title asc) {
+// get all job categories for opportunities page
+export const fetchAllJobCategories = groq`
+    *[_type == "jobCategory"] | order(title asc) 
+    {
         _id,
         title,
         "slug": slug.current,
         description,
         "categoryImage": categoryImage.asset->url
+    }
+`;
+
+// get all jobs under their category(list view).
+export const fetchJobsByCategory = groq`
+    *[_type == "jobs" && active == true && category->slug.current == $slug] | order(_createdAt desc)
+    {
+        _id,
+        jobTitle,
+        "slug": slug.current,
+        "jobLocation": jobLocation.city + ", " + jobLocation.state,
+        type,
+        "category": category->title,
+    }
+`;
+
+// get individual job details
+export const fetchJobBySlug = groq`
+    *[_type == "jobs" && slug.current == $slug][0]
+    {
+        _id,
+        jobTitle,
+        "slug": slug.current,
+        "jobLocation": jobLocation.city + ", " + jobLocation.state,
+        type,
+        jobDescription,
+        "category": category->title,
+        "categorySlug": caetgorySlug->slug.current,
     }
 `;
---
+++ nextjs-weareiw/src/types/job.ts
@@ -0,0 +1,7 @@
+export type jobCategory = {
+    _id: string;
+    title: string;
+    slug: string;
+    description: string;
+    categoryImage: string;
+};
