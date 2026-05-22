98cc8ee712818470ae1a4f796ad4aacd7688ad21
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-05-22 10:44:42 -0500

    opportunities page: added types file for job. starting work on opportunities grid

--- nextjs-weareiw/src/app/opportunities/page.tsx
+++ nextjs-weareiw/src/app/opportunities/page.tsx
@@ -2,14 +2,20 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
 import Hero from "@/components/Hero";
-import Blurb from "@/components/Blurb";
+import Card from "@/components/Card";
+
+import { client } from "@/sanity/lib/client";
+import { fetchAllJobCategories } from "@/sanity/queries/job";
+import { JobCategory } from "@/types/job";
 
 export const metadata: Metadata = {
   title: "Opportunities - IW Technologies",
   description: "Opportunities at IW Technologies.",
 };
 
-export default function OpportunitiesPage() {
+export default async function OpportunitiesPage() {
+    const categories: JobCategory[] = await client.fetch(fetchAllJobCategories);
+
     return (
         <div>
             <section className="h-fit text-white">
@@ -32,6 +38,26 @@ export default function OpportunitiesPage() {
                     </>
                     }
                 />
+            </section>
+
+            <section className="w-full bg-brand-black">
+                <div className="w-full py-14 rounded-t-3xl bg-background">
+                    <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-5">
+                        {categories.map((category) => (
+                            <Link key={category._id} 
+                            href={`/careers/opportunities/${category.slug}`}
+                            className="group">
+                                <Card
+                                    text={
+                                        <>
+                                            <h3 className="hover:text-brand-aqua">{category.title}</h3>
+                                        </>
+                                    }
+                                    />
+                            </Link>
+                        ))}
+                    </div>
+                </div>
             </section>
         </div>
     )
--- nextjs-weareiw/src/types/job.ts
+++ nextjs-weareiw/src/types/job.ts
@@ -1,7 +1,21 @@
-export type jobCategory = {
+export type JobCategory = {
     _id: string;
     title: string;
     slug: string;
     description: string;
     categoryImage: string;
+};
+
+export type Job = {
+    _id: string;
+    jobTitle: string;
+    slug: string;
+    location: string;
+    type: string;
+    category: string;
+};
+
+export type JobDetail = Job & {
+    jobDescription: any[];
+    categorySlug: string;
 };
