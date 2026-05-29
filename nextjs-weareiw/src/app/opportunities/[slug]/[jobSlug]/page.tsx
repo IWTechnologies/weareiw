4b20db2de0d3342c380349959b5ae7204704588f
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-05-29 14:25:20 -0500

    jobSlug: updated styling and structure of top section. added new icon asset

--- nextjs-weareiw/src/app/opportunities/[slug]/[jobSlug]/page.tsx
+++ nextjs-weareiw/src/app/opportunities/[slug]/[jobSlug]/page.tsx
@@ -13,22 +13,52 @@ export default async function JobDetailPage({ params }: { params: { slug: string
 
     return (
         <div>
-            <section className="w-[80%] mx-auto py-14 text-brand-black">
-                <a href={`/opportunities/${job.categorySlug}`}
-                className="hover:text-brand-aqua">← Go back</a>
-                <h1>{job.jobTitle}</h1>
-                <div className="flex gap-4 mt-2 text-sm">
-                    <p>{job.city}</p>
-                    <p>{job.state}</p>
-                    <p>{job.type}</p>
-                    <p>{job.category}</p>
-                </div>
-                <div className="mt-10">
-                    <h2>Job Summary:</h2>
-                    <div className="prose max-w-none">
-                        <PortableText value={job.jobDescription} />
+            <section className="w-full py-14 text-brand-black">
+                {/* put the below structure within a flex-col div */}
+                <div className="w-[70%] mx-auto flex flex-col gap-3">
+                    <div className="w-fit flex flex-col gap-5">
+                        <a href={`/opportunities/${job.categorySlug}`}
+                        className="w-fit h-fit px-4 py-2 text-center 
+                        border-3 border-brand-black bg-brand-black text-white rounded-md
+                        hover:bg-transparent hover:text-brand-black active:bg-transparent active:text-brand-black
+                        focus-visible:outline-4 focus-visible:outline-offset-2 focus-visible:outline-brand-aqua
+                        transition duration-300 ease-in-out">
+                            ← Go back
+                        </a>
+
+                        <h1 className="w-fit border-b-4 border-brand-aqua">{job.jobTitle}</h1>
+                        <div className="flex flex-col md:flex-row items-start md:items-center gap-6 mt-2">
+                            <span className="flex items-center gap-1">
+                                <img src="/careerImages/icons/location-dot-solid-full.svg" 
+                                alt="location pin icon" height="25" width="25"
+                                className="" />
+                                <p>{job.city},</p>
+                                <p>{job.state}</p>
+                            </span>
+                            <span className="flex items-center gap-1">
+                                <img src="/careerImages/icons/clock-solid-full.svg" 
+                                alt="location pin icon" height="25" width="25"
+                                className="" />
+                                <p>{job.type}</p>
+                            </span>
+                            <span className="flex items-center gap-1">
+                                <img src="/careerImages/icons/user-solid-full.svg" 
+                                alt="location pin icon" height="25" width="25"
+                                className="" />
+                                <p>{job.category}</p>
+                            </span>
+                        </div>
+                    </div>
+                    
+
+                    <div className="mt-10">
+                        <h2>Job Summary:</h2>
+                        <div className="prose max-w-none">
+                            <PortableText value={job.jobDescription} />
+                        </div>
                     </div>
                 </div>
+                
             </section>
         </div>
     );
