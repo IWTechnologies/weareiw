e5588cf547617158ec4b05107202f114a67b9d5a
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-06-11 15:44:49 -0500

    queries: product queries added

---
+++ nextjs-weareiw/src/sanity/queries/product.ts
@@ -0,0 +1,36 @@
+import { groq } from "next-sanity";
+
+// get all product categories for products page filter
+export const fetchAllProductCategories = groq`
+    *[_type == "productCategory"] | order(title asc) {
+        _id,
+        title,
+        "slug": slug.current,
+    }
+`;
+
+// get all products with their category
+export const fetchAllProducts = groq`
+    *[_type == "products"] | order(title asc) {
+        _id,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        "category": category->title,
+        "categorySlug": category->slug.current
+    }
+`;
+
+// get all individual product details
+export const fetchAllProductsBySlug = groq`
+    *[_type == "products"] | order(title asc) {
+        _id,
+        name,
+        "slug": slug.current,
+        "image": image.asset->url,
+        "category": category->title,
+        "categorySlug": category->slug.current,
+        description,
+        specs
+    }
+`;
