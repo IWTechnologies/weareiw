17077d00cdf5e26ed44152e0fce5aad70050a15c
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-06-29 09:40:47 -0500

    news: page, slug page, queries, and types created and added.

---
+++ nextjs-weareiw/src/app/resources/news/[slug]/page.tsx
@@ -0,0 +1,157 @@
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+import { client } from "@/sanity/lib/client";
+import { fetchAllNewsPosts, fetchAllNewsBySlug } from "@/sanity/queries/news";
+import { NewsDetail } from "@/types/news";
+import { PortableText } from "next-sanity";
+
+import { PortableTextComponents } from "@portabletext/react";
+import { createImageUrlBuilder } from "@sanity/image-url";
+
+const builder = createImageUrlBuilder(client);
+
+export function urlFor(source: string) {
+  return builder.image(source);
+}
+
+// tables are custom types and this allows any tables in the body to be rendered.
+// images are custom types and this allows any images in the body to be rendered.
+// these are passed into the portable text component
+const components: PortableTextComponents = {
+    types: {
+        table: ({ value }) => {
+            if (!value?.rows || value.rows.length === 0) return null;
+
+            // Extract the first row as the header, and the rest as body rows
+            const [headerRow, ...bodyRows] = value.rows;
+
+            return (
+                <div className="w-full overflow-x-auto">
+                    <table className="w-full border-collapse my-6">
+                        <thead>
+                            <tr>
+                                {headerRow.cells?.map((cell: string, cellIndex: number) => (
+                                    <th key={cellIndex} className="border border-brand-black/20 p-3">
+                                        {cell}
+                                    </th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {bodyRows.map((row: any, rowIndex: number) => (
+                                <tr key={rowIndex}>
+                                    {row.cells.map((cell: string, rowIndex: number) => (
+                                        <td key={rowIndex} className="border border-brand-black/20 p-3">
+                                            {cell}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            );
+        },
+        image: ({ value }) => {
+            if (!value?.asset?._ref) return null;
+
+            return (
+                <figure className="">
+                    <img
+                        src={urlFor(value).width(300).fit('max').auto('format').url()}
+                        alt={value.alt || 'Blog image'}
+                        className="max-w-full h-auto"
+                        loading="lazy"
+                    />
+                </figure>
+            );
+        },
+    },
+};
+
+export async function generateMetadata(
+    { params }: { params: { slug: string } }
+): Promise<Metadata> {
+    const { slug } = await params;
+    const news: NewsDetail = await client.fetch(fetchAllNewsBySlug, { slug });
+
+    if (!news) {
+        return {
+            title: "News Article Not Found - IW Technologies",
+        };
+    }
+
+    return {
+        title: `${news.seoTitle} - IW Technologies`,
+        description: `${news.metaDescription}`,
+        openGraph: {
+            title: news.seoTitle,
+            description: news.metaDescription,
+            images: [
+                {
+                    url: news.image
+                }
+            ],
+        },
+    }
+}
+
+// pre-render pages at build time
+export async function generateStaticParams() {
+    const news = await client.fetch(fetchAllNewsPosts);
+    
+    return news.map((blog: { slug: string }) => ({
+        slug: blog.slug,
+    }));
+}
+
+export default async function BlogDetailPage({ params }: { params: { slug: string } }) {
+    const { slug } = await params;
+    const news: NewsDetail = await client.fetch(fetchAllNewsBySlug, { slug });
+
+    if (!news) notFound();
+
+    return (
+        <div>
+            <section className="w-full my-16 text-brand-black">
+                <div className="w-[70%] mx-auto flex flex-col gap-10">
+                    <a href="/resources/blog"
+                        className="w-fit h-fit px-4 py-2 text-center 
+                        border-3 border-brand-black bg-brand-black text-white rounded-md
+                        hover:bg-transparent hover:text-brand-black active:bg-transparent active:text-brand-black
+                        focus-visible:outline-4 focus-visible:outline-offset-2 focus-visible:outline-brand-aqua
+                        transition duration-300 ease-in-out">
+                            ← Go back
+                        </a>
+                    
+                    <div className="w-full flex flex-col gap-8">
+                        {/* only render if blog image exists for blog */}
+                        {news.image && (
+                            <div className="w-full lg:w-[70%] mx-auto">
+                                <img
+                                    src={news.image}
+                                    alt={news.altText}
+                                    className="w-full h-auto rounded-2xl object-contain"
+                                />
+                            </div>
+                        )}
+
+                        <div className="w-full flex flex-col gap-4">
+                            <div className="w-full flex flex-col pl-2 border-brand-aqua border-l-4 gap-2">
+                                <h1>{news.title}</h1>
+                            </div>
+
+                            {/* only render if blog content exists for blog */}
+                            {news.body && (
+                                <div className="prose prose-ink-black prose-figure:m-0 max-w-none">
+                                    <PortableText value={news.body} components={components} />
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
--- nextjs-weareiw/src/app/resources/news/page.tsx
+++ nextjs-weareiw/src/app/resources/news/page.tsx
@@ -3,37 +3,37 @@ import Link from "next/link";
 import Card from "@/components/Card";
 
 import { client } from "@/sanity/lib/client";
-import { fetchAllBlogPosts } from "@/sanity/queries/blog";
-import { Blog, BlogDetail } from "@/types/blog";
+import { fetchAllNewsPosts } from "@/sanity/queries/news";
+import { News, NewsDetail } from "@/types/news";
 
 export const metadata: Metadata = {
-  title: "Blog - IW Technologies",
+  title: "News - IW Technologies",
   description: "From installation to ITAD, new to refurbished POS hardware, IW Technologies offers end-to-end technology solutions..",
 };
 
 
-export default async function BlogPage() {
-    const blogs: Blog[] = await client.fetch(fetchAllBlogPosts);
+export default async function NewsPage() {
+    const news: News[] = await client.fetch(fetchAllNewsPosts);
 
     return (
         <div>
             <section className="w-full my-16 text-brand-black">
                 <div className="w-[70%] mx-auto flex flex-col gap-10">
                     <h2 className="">
-                        Explore our blog
+                        Explore the latest news
                     </h2>
                     <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-                        {blogs.map((blog) => (
-                            <Link key={blog._id} href={`/resources/blog/${blog.slug}`}
+                        {news.map((news) => (
+                            <Link key={news._id} href={`/resources/news/${news.slug}`}
                             className="group w-full flex flex-col p-4 rounded-3xl bg-brand-black overflow-hidden
                             transition duration-300 focus-visible:outline-4 focus-visible:outline-offset-2 focus-visible:outline-brand-aqua">
                                 {/* display blog image, if not blog image. use fallback image */}
                                 <div className="w-full h-48 rounded-3xl overflow-hidden">
-                                    <img src={blog.image || "/IWT_Logomark_White.png"} alt={blog.altText}
+                                    <img src={news.image || "/IWT_Logomark_White.png"} alt={news.altText}
                                     className="w-full h-full object-cover" />
                                 </div>
                                 <h4 className="w-full py-4 text-center transition duration-300 text-white group-hover:text-brand-aqua">
-                                    {blog.title}
+                                    {news.title}
                                 </h4>
                             </Link>
                         ))} 
