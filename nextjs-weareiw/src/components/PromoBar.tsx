3bb6c6e59ebbde3b84eebb98c4f9a445c43ee499
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-07-09 10:07:56 -0500

    PromoBar: updated text

--- nextjs-weareiw/src/components/PromoBar.tsx
+++ nextjs-weareiw/src/components/PromoBar.tsx
@@ -4,11 +4,11 @@ export default function PromoBar() {
     return (
         <div className="w-full flex justify-center text-center bg-brand-gold text-brand-black">
             <div className="w-[80%] md:w-full mx-auto flex flex-col md:flex-row items-center md:justify-center">
-                <span className="w-fit">50 Years. Built on Trust. Earned in the Field...</span>
+                <span className="w-fit">50 Years. Built on Trust. Earned in the Field. </span>
                 <Link href="/about/our-story/" 
                 target="_blank" 
                 className="w-fit font-medium underline hover:text-white hover:cursor-pointer">
-                    See our story
+                    See our story.
                 </Link>
             </div>
             
