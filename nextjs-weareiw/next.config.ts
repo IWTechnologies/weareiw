9f1c44ac7a48889953704f427a282752c9f61470
Antonio Saenz <asaenz@illinoiswholesale.com>
2026-09-10 15:41:17 -0500

    TeamGrid: updated how team member bio is displayed, using css instead of js. next config: added redirects for old links.

--- nextjs-weareiw/next.config.ts
+++ nextjs-weareiw/next.config.ts
@@ -10,6 +10,86 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/our-people",
+        destination: "/about#our-people",
+        permanent: true,
+      },
+      {
+        source: "/our-people/",
+        destination: "/about#our-people",
+        permanent: true,
+      },
+      {
+        source: "/blog",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/blog/",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/blogs",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/blogs/",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      // wildcards for all blog posts instead of trying to target individual posts
+      {
+        source: "/blog/:slug",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/blogs/:slug",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/news/:slug",
+        destination: "/resources/news",
+        permanent: true,
+      },
+      {
+        source: "/news",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/news/",
+        destination: "/resources/blog",
+        permanent: true,
+      },
+      {
+        source: "/sell",
+        destination: "/services/disposition",
+        permanent: true,
+      },
+      {
+        source: "/sell/",
+        destination: "/services/disposition",
+        permanent: true,
+      },
+      {
+        source: "/catalog",
+        destination: "https://shop.weareiw.com/",
+        permanent: true,
+      },
+      {
+        source: "/catalog/",
+        destination: "https://shop.weareiw.com/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
